628a434d3c854bf4ccb33492643d2ad0aae2d291
Pooja Patil <poojapatil010677@gmail.com>
2020-06-12 23:27:38 +0530

    Updates

---
+++ precache-manifest.38fb133681e42725fddb33abc964420a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e5b377604b9fdf1a49a3abecda2f8e",
+    "url": "/Make-List-App-React/index.html"
+  },
+  {
+    "revision": "a3dfb05be41d14f78532",
+    "url": "/Make-List-App-React/static/css/main.9692c897.chunk.css"
+  },
+  {
+    "revision": "bb5c9bc06ca5aa693a14",
+    "url": "/Make-List-App-React/static/js/2.25402f46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Make-List-App-React/static/js/2.25402f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3dfb05be41d14f78532",
+    "url": "/Make-List-App-React/static/js/main.7d8baf20.chunk.js"
+  },
+  {
+    "revision": "dabe06503f698a07dac9",
+    "url": "/Make-List-App-React/static/js/runtime-main.3d476951.js"
+  }
+]);
